3ff9aeb65e9994403e94ff5a26d1d4da98d25a8c
harshalburadkar <harshal.b06@gmail.com>
2025-01-29 00:14:06 +0530

    test

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+let captchChecked = false;
+
 function beforesubmit() {
-    let outputdate = document.querySelector(".outputdate");
+    if(captchChecked) {
+        let outputdate = document.querySelector(".outputdate");
     let inputdate = document.querySelector(".inputdate");
     console.log("inputdate.value", inputdate.value); //string --> date (en_IN)
     
@@ -8,7 +11,16 @@ function beforesubmit() {
     outputdate.value = formattedDate;
     console.log("outputdate.value", outputdate.value);
 
+    } else {
+        alert("Please check the reCAPTCH box to Submit the lead");
+        event.preventDefault();
+    }
 }
 
 
  function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500); 
+
+
+ function captchsuccess() {
+    captchChecked = true;
+ }
